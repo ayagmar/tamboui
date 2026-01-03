42cc359fae739d2be1c3e946409b0fd27e9a1fe6
Max Rydahl Andersen <max@xam.dk>
2026-01-03 01:20:35 +0100

    fix: add verification of java 8 in the core libs + fix the paragraph code using java 11

--- buildSrc/src/main/kotlin/dev.tamboui.java-library.gradle.kts
+++ buildSrc/src/main/kotlin/dev.tamboui.java-library.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     `java-library`
     id("dev.tamboui.publishing")
+    id("ru.vyarus.animalsniffer")
 }
 
 dependencies {
     val libs = versionCatalogs.named("libs")
     testImplementation(platform(libs.findLibrary("junit.bom").orElseThrow()))
     testImplementation(libs.findBundle("testing").orElseThrow())
+    signature("org.codehaus.mojo.signature:java18:1.0@signature")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("ru.vyarus.animalsniffer") version "2.0.0"
+    }
+}
+
 rootProject.name = "tamboui-parent"
 
 include(
