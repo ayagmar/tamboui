d5c024d4df9b2c9a3f0dfe15ffe6fd7c40343045
Cedric Champeau <cedric.champeau@gmail.com>
2026-02-18 08:30:49 +0100

    Make the panama backend "compatible" with Java 8

--- tamboui-panama-backend/build.gradle.kts
+++ tamboui-panama-backend/build.gradle.kts
@@ -17,3 +17,10 @@ tasks.withType<Test> {
 dependencies {
     api(projects.tambouiCore)
 }
+
+// This project is compiled with Java 22, but at runtime we want
+// it to be compatible with applications that ship with multiple
+// backends, in which case it will be silently ignored on older JVMs.
+configurations.runtimeElements {
+    attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 8)
+}
